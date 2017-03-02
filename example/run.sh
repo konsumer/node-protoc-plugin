1655ad1e621a6ec3d8aabd1187458b823cdc58ef
David Konsumer <konsumer@jetboystudio.com>
2017-03-01 21:32:28 -0800

    more docs for example

--- example/run.sh
+++ example/run.sh
@@ -1,17 +1,23 @@
 #!/bin/bash
 
+# here is how to run all the examples:
+
+# get dir of this script
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-mkdir generated 2> /dev/null
+# make a place for utput
+mkdir ${DIR}/../generated 2> /dev/null
 
+# test out simple JSON logger
 protoc\
   --plugin=${DIR}/protoc-gen-logger \
   -I ${DIR}/../proto \
   ${DIR}/../proto/helloworld.proto \
-  --logger_out=generated
+  --logger_out=${DIR}/../generated
 
+# test out more advanced JSON logger
 protoc\
   --plugin=${DIR}/protoc-gen-extendedlogger \
   -I ${DIR}/../proto \
   ${DIR}/../proto/helloworld.proto \
-  --extendedlogger_out=generated
+  --extendedlogger_out=${DIR}/../generated
